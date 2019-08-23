3c7f88193a67ab141973cbeb4f3596e93921d703
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-23 16:02:08 +1000

    choosing fighters and defenders is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,29 +5,54 @@ var airElement;
 
 var currentAP;
 var currentFighter;
+var currentDefender;
+
+var fighterChosen;
+var defenderChosen;
 
 //start the game. Or restart.
 function startGame(){
     $(".currentFighter").text("Choose your fighter");
+    $(".currentDefender").text("Choose your fighter");
     waterElement = 100;
     earthElement = 100;
     fireElement = 100;
     picElement = 100;
     
+    fighterChosen = false;
+    defenderChosen = false;
     currentAP = 0;
 
 }
 
 //setting the fighter
-
-    $(".elementChar").on("click", function(){
+function setFighter(){
+if (!fighterChosen){
         currentFighter = this.id;
-        $("currentFighter").html(currentAP);
+        $(".currentFighter").html(this);
         
-    })
+        $(".currentDefender").text("Choose your defender");
+        fighterChosen = true;
+
+}
+}
+
+//setting the defender
+if (!defenderChosen){
+    $(".elementChar").on("click", function(){
+        currentDefender = this.id;
+        $(".currentDefender").html(this);
+        
+        defenderChosen = true;
+
+})
+}
 
 
 
 //Play
 startGame();
+console.log(fighterChosen)
 
+$(".elementChar").on("click", setFighter);
+console.log(fighterChosen)
